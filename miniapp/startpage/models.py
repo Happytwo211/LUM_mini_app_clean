8f825de2d7fa956e8e8925d9c153c32a05182ebc
DanielDitrikh <daniel.ditrikh@mail.ru>
2025-06-27 00:11:42 +0300

    Добавлена проверка на приобретение жкскурсии

--- miniapp/startpage/models.py
+++ miniapp/startpage/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse, request
 
 class Tour(models.Model):
     tour_name = models.CharField(max_length=300, default='Экскурсия')
@@ -15,11 +16,24 @@ class Tour(models.Model):
         return f'{self.tour_name} : {self.tour_desc[:20]}'
 
 class UserStats(models.Model):
-    user_stats_to_user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    user_own_tours = models.ForeignKey(to=Tour, on_delete=models.CASCADE, null=True)
-    user_bonus_currency = models.IntegerField(null=True)
-    user_promocode = models.CharField(max_length=20, null=True)
-
+    user_own_tours = models.ForeignKey(to=Tour, default=None, on_delete=models.CASCADE, unique=True)
+    user_stats_to_user = models.ForeignKey(to=User, default=None, on_delete=models.CASCADE)
     def __str__(self):
-        return f'{self.user_stats_to_user}'
+        return f'{self.user_stats_to_user}:{self.user_own_tours}'
+
+    def check_tour_ownership(request, tour_id ):
+        try:
+            tour = get_object_or_404(Tour, id=tour_id)
+            user_stats  = UserStats.objects.filter(
+                user_stats_to_user=request.user,
+                user_own_tours=tour
+            )
+            is_owned = user_stats.exists()
+
+            return JsonResponse({
+                'is_tour_owned': is_owned
+            })
+
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
 
--- miniapp/startpage/urls.py
+++ miniapp/startpage/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from .views import ToursList, UserProfile, ToursDetail
-
+from . import views
 
 urlpatterns = [
     path('tours/', ToursList.as_view(), name='tours_list'),
     path('profile/<int:pk>', UserProfile.as_view(), name='profile'),
     path('tours/<int:pk>', ToursDetail.as_view(), name='tour_detail'),
+    path('check-tour/<int:tour_id>/', views.UserStats.check_tour_ownership,name='check_tour'),
 ]
 
 
