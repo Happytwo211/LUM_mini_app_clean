d4f5cdca21bbef74dd061fa623ec443021602971
DanielDitrikh <daniel.ditrikh@mail.ru>
2025-06-30 03:57:07 +0300

    Добавлена плашка экскурсии

--- miniapp/startpage/admin.py
+++ miniapp/startpage/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Tour, UserStats
+from .models import Tour, UserStats, ContentType
 admin.site.register(Tour)
 admin.site.register(UserStats)
+admin.site.register(ContentType)
--- miniapp/startpage/models.py
+++ miniapp/startpage/models.py
@@ -4,6 +4,13 @@ from django.shortcuts import get_object_or_404
 from django.http import JsonResponse, request
 
 class Tour(models.Model):
+
+    CONTENTYPE = 'Экскурсия'
+
+    tour_content_type = models.CharField(default=CONTENTYPE, editable=False)
+    tour_time_add = models.TimeField(auto_now=True)
+    tour_date_add = models.DateField(auto_now_add=True)
+
     tour_name = models.CharField(max_length=300, default='Экскурсия')
     tour_desc = models.TextField(max_length=2000, default='Описание экскурсии')
     tour_rating = models.IntegerField(default=10)
@@ -37,3 +44,20 @@ class UserStats(models.Model):
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
 
+
+class ContentType(models.Model):
+    content_type_tour = 'TOUR'
+    content_type_game = 'GAME'
+    content_type_quiz = 'QUIZ'
+
+    POSITION = [
+        (content_type_tour,'Экскурсия'),
+        (content_type_game, 'Игра'),
+        (content_type_quiz, 'Квиз')
+    ]
+
+    choose = models.CharField(max_length=4, choices=POSITION, null=False)
+
+
+    def __str__(self):
+        return f'{self.choose}'
